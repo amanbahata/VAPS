a9370776307b5cb8c077eba1d98c7e0affc2cf20
Aman Bahata <bahataaman@gmail.com>
2017-04-17 11:46:39 +0100

    Adds form structure for a new application

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -6,14 +6,16 @@
 module.exports.home = function (req, res) {
     res.render('index', {
         title: 'WELCOME,',
-        information: ' visiting Ethiopia for pleasure has never been easier.'
+        information: ' visiting Ethiopia for pleasure has never been easier.' +
+                        'Home to vast and diverse natural wild life reserves, blessed with tropical, all year round ' +
+                        'summer weather and a diverse cultural heritage.'
     });
 };
 
 
 
 module.exports.newApplication = function (req, res) {
-    res.render('index', {
-        title: 'New application page'
+    res.render('new_application', {
+        title: 'New application'
     });
 };
